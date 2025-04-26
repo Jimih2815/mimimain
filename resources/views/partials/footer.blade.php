44eedc557c5fa258e314d07aaafb12f5d8d28042
jimih2815 <truonggiang2815@gmail.com>
2025-04-26 17:19:52 +0700

    sua footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,48 +1,64 @@
 <footer class="bg-light pt-5">
-  <div class="container">
+  <div class="container footer-container">
 
     {{-- 1. Links + Social columns --}}
-    <div class="row">
-      <div class="col-md-3 mb-4">
-        <h5>Resources</h5>
+    <div class="footer-danh-sach-cont">
+      <div class="footer-danh-sach">
+        <h5>Giới thiệu</h5>
         <ul class="list-unstyled">
-          <li><a href="#">Find A Store</a></li>
-          <li><a href="#">Become A Member</a></li>
-          <li><a href="#">Running Shoe Finder</a></li>
-          <li><a href="#">Send Us Feedback</a></li>
+          <li><a href="#">Về Mimi Shop</a></li>
+          <li><a href="#">Tầm nhìn, sứ mệnh</a></li>
+          <li><a href="#">Cam kết</a></li>
+          <li><a href="#">Tin tức</a></li>
         </ul>
       </div>
-      <div class="col-md-3 mb-4">
-        <h5>Help</h5>
+      <div class="footer-danh-sach">
+        <h5>Liên hệ</h5>
         <ul class="list-unstyled">
-          <li><a href="#">Get Help</a></li>
-          <li><a href="#">Order Status</a></li>
-          <li><a href="#">Delivery</a></li>
-          <li><a href="#">Returns</a></li>
-          <li><a href="#">Payment Options</a></li>
-          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">Địa chỉ</a></li>
+          <li><a href="#">Hotline</a></li>
+          <li><a href="#">Email</a></li>
+          <li>9:00 - 17:30</li>
         </ul>
       </div>
-      <div class="col-md-3 mb-4">
-        <h5>Company</h5>
+      <div class="footer-danh-sach">
+        <h5>Chính sách</h5>
         <ul class="list-unstyled">
-          <li><a href="#">About MimiMain</a></li>
-          <li><a href="#">News</a></li>
-          <li><a href="#">Careers</a></li>
-          <li><a href="#">Investors</a></li>
-          <li><a href="#">Sustainability</a></li>
-          <li><a href="#">Report a Concern</a></li>
+          <li><a href="#">Chính sách đổi trả</a></li>
+          <li><a href="#">Chính sách bảo hành</a></li>
+          <li><a href="#">Chính sách vận chuyển giao hàng</a></li>
+          <li><a href="#">Chính sách bảo mật thông tin</a></li>
         </ul>
       </div>
-      <div class="col-md-3 mb-4">
+      <div class="footer-danh-sach">
+        <h5>Hỗ trợ khách hàng</h5>
+        <ul class="list-unstyled">
+          <li><a href="#">Câu hỏi thường gặp (FAQ)</a></li>
+          <li><a href="#">Hướng dẫn đặt hàng</a></li>
+          <li><a href="#">Theo dõi đơn hàng</a></li>
+          <li><a href="#">Hướng dẫn thanh toán</a></li>
+        </ul>
+      </div>
+      <div class="footer-danh-sach">
         <h5>Theo dõi chúng tôi</h5>
         <div class="d-flex">
-          <a href="#" class="text-dark me-3"><i class="fab fa-facebook-f fa-lg"></i></a>
-          <a href="#" class="text-dark me-3"><i class="fab fa-instagram fa-lg"></i></a>
-          <a href="#" class="text-dark me-3"><i class="fab fa-twitter fa-lg"></i></a>
-          <a href="#" class="text-dark me-3"><i class="fab fa-pinterest fa-lg"></i></a>
-          <a href="#" class="text-dark me-3"><i class="fab fa-tiktok fa-lg"></i></a>
-          <a href="#" class="text-dark"><i class="fab fa-youtube fa-lg"></i></a>
+        
+        <a href="https://www.tiktok.com/@tiemhoamimi1" target="_blank" class="text-dark me-3">
+          <i class="fab fa-tiktok fa-lg"></i>
+        </a>
+        <a href="https://facebook.com/yourpage" target="_blank" class="text-dark me-3">
+          <i class="fab fa-facebook-f fa-lg"></i>
+        </a>          
+        <a href="https://www.threads.net/@yourprofile" target="_blank" class="text-dark me-3">
+          <i class="fa-brands fa-threads fa-lg"></i>
+        </a>
+          <a href="https://zalo.me/0354235669" target="_blank" class="text-dark me-3">
+            <img src="/logochat/logo-zalo.png" alt="Zalo" style="width: 20px; margin-bottom: 0.4rem; height: 20px;">
+          </a>
+          <a href="https://shopee.vn/thanhhuyen2461" target="_blank" class="text-dark me-3">
+            <img src="/logochat/shopee.png" alt="Zalo" style="width: 20px; margin-bottom: 0.4rem; height: 20px;">
+          </a>
+
         </div>
       </div>
     </div>
@@ -63,11 +79,12 @@
       {{-- Map (≈67% width) --}}
       <div class="col-md-8 mb-4">
         <div class="map-container mb-2">
-          <iframe
+          <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.034928716555!2d105.7753423154153!3d21.02158289258868!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab6f4c9e7a1f%3A0x123456789abcdef!2zVGllbSBob2EgTWltZQ!5e0!3m2!1svi!2s!4v1234567890123"
             frameborder="0"
             allowfullscreen
             loading="lazy"
+            style="width:100%;"
           ></iframe>
         </div>
       </div>
@@ -75,7 +92,7 @@
 
 
     <div class="text-center pb-3">
-      &copy; {{ date('Y') }} MimiMain. All rights reserved.
+      &copy; 2023 MimiShop. All rights reserved.
     </div>
 
   </div>
